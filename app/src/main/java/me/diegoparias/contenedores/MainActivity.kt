7353e6c8afd1c530fa40af9d2b6c8c5d6edf2f5c
Diego Parias <dparias@unab.edu.co>
2026-02-25 05:54:28 -0800

    first commit

---
+++ app/src/main/java/me/diegoparias/contenedores/MainActivity.kt
@@ -0,0 +1,113 @@
+package me.diegoparias.contenedores
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.diegoparias.contenedores.ui.theme.ContenedoresTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ContentScreen(){
+
+    Column(modifier = Modifier
+        .background(Color.LightGray)
+        .padding(all=18.dp)
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    )
+    {
+
+
+        Row(modifier = Modifier
+            .background(Color.Gray)
+            .padding(all = 18.dp)
+            . fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+
+        ){
+            Text(
+                text = "Inicio",
+                fontSize = 25.sp,
+                modifier =
+                    Modifier
+                        .background(Color.Red)
+                        .padding(all = 8.dp)
+            )
+            Text(
+                text = "Perfil",
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(all = 8.dp)
+
+            )
+            Text(
+                text = "Configuracion",
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .background(Color.Cyan)
+                    .padding(all = 8.dp)
+
+            )
+        }
+
+        Text(
+            text = "Configuracion",
+            fontSize = 25.sp,
+            modifier = Modifier
+                .background(Color.Cyan)
+                .padding(all = 8.dp)
+
+        )
+        Text(
+            text = "Configuracion",
+            fontSize = 25.sp,
+            modifier = Modifier
+                .background(Color.Cyan)
+                .padding(all = 8.dp)
+
+        )
+        Text(
+            text = "Configuracion",
+            fontSize = 25.sp,
+            modifier = Modifier
+                .background(Color.Cyan)
+                .padding(all = 8.dp)
+
+        )
+
+    }
+
+
+
+}
